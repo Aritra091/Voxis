ea486305cef294a4477e29c75ac379a2ba69608e
Aritra Chattopadhyay <aritrachattopadhyay200@gmail.com>
2025-04-22 23:42:43 +0530

    Update

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('assist/', views.assist, name='assist'),            # Home page → index.html
-    path('ask-groq/', views.ask_groq, name='ask'),  # POST endpoint
-]
